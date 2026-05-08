a74d18c41fa6b36e9fb45934fa788efd3012a67f
Lê Việt Tân <leviettan000@gmail.com>
2026-05-08 23:42:42 +0700

    refactor(frontend): update app entry and global styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "@/assets/styles/index.css";
+import App from "./App.tsx"
 
 if (window.location.hash === "#_=_" || window.location.hash === "#_") {
     const cleanUrl =
