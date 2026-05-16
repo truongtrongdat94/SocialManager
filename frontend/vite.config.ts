a8dc32d6be02e6b5021931a3469ae5e81050b899
BaThuanPham <22521447@gm.uit.edu.vn>
2026-05-16 18:50:34 +0700

    chore(frontend): add package and build config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      tailwindcss()
+  ],
   build: {
     outDir: '../social-manager-backend/src/main/resources/static',
     emptyOutDir: true,
@@ -16,4 +21,9 @@ export default defineConfig({
       },
     },
   },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src/")
+    }
+  }
 })
